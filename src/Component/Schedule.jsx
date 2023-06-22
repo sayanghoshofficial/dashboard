2f4299eefd54a37c40ee425c1d7669df73d80315
Sayan Ghosh <sayanghosh379@gmail.com>
2023-06-22 16:42:21 +0530

    Create Shedule div

--- src/Component/Schedule.jsx
+++ src/Component/Schedule.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React from "react";
 import "../Style/Dashboard.css";
 
 const Schedule = () => {
   return (
-    <div className='scheduleCointainer'>
-        <div className='headingSchedule'></div>
-        <div><div></div><div></div></div>
-      
+    <div className="scheduleCointainer">
+      <div className="headingSchedule">
+        <h3>Today's schedule</h3>
+        <p>See All{" >"}</p>
+      </div>
+      <div className="meetingTime">
+        <div className="fastMeeting">
+          <h5>Meeting with suppliers from Kuta Bali</h5>
+          <p>14.00-15.00</p>
+          <small>at Sunset Road, Kuta ,Bali</small>
+        </div>
+        <div>
+          <div className="senondMeeting">
+            <h5>Check Operation at Giga Factory 1</h5>
+            <p>18.00-20.00</p>
+            <small>at Central Jakarta</small>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Schedule
+export default Schedule;
--- src/Component/index.jsx
+++ src/Component/index.jsx
@@ -2,5 +2,6 @@ import DashBoardLeft from "./DashBoardLeft";
 import DashBoardNav from "./DashBoardNav";
 import AnalyticsBox from "./AnalyticsBox";
 import Schedule from "./Schedule";
+import Activities from "./Activities";
 
-export { DashBoardLeft, DashBoardNav, AnalyticsBox, Schedule };
+export { DashBoardLeft, DashBoardNav, AnalyticsBox, Schedule, Activities };
